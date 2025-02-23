46e0e23c8a3b9ba90036d6843a30353586cafcdb
egoist <tianyuma4869@gmail.com>
2025-02-23 20:12:58 +1100

    title + endpoint

--- apps/tokomo_FE/next.config.js
+++ apps/tokomo_FE/next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:8080/api/:path*'
+        destination: 'http://api.tokomoapp.org/api/:path*'
       }
     ];
   }
